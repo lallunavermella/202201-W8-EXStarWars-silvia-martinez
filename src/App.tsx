672a4f13ad2b39938e6516172f34fcd7ff25439c
Silvia Martinez <sil@silvia.com>
2022-03-04 14:01:51 +0100

    Add getStarShips

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
+export interface Starships {
+  count: number;
+}
+
+function App(): JSX.Element {
+  const [countShips, setCountShips] = useState<Number[]>();
+
+  useEffect(() => {
+    (async () => {
+      const getStarShips = async () => {
+        const response = await fetch("http://swapi.dev/api/starships/");
+        const starships = await response.json();
+        return starships;
+      };
+      const ships = await getStarShips();
+      setCountShips(ships.count);
+    })();
+  }, []);
+
   return (
     <div className="App">
       <div id="background-container">
         <header id="header-title">Star Wars Test</header>
         <main id="main-container">
           <h2>Starships:</h2>
-          <p id="total-ships">Total ships: </p>
+          <p id="total-ships">Total ships: {countShips} </p>
+
           <h2>Starships by class:</h2>
         </main>
       </div>
